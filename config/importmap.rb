9bcceb13a2285c74636008a84744414784ba72c5
root <annie@ai-stocktrading.com>
2025-08-23 21:40:04 -0500

    editor

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 # config/importmap.rb
 pin "filters_toggle", to: "filters_toggle.js"
+pin "wysiwyg", to: "wysiwyg.js"
